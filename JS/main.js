53b296d905f2bbb3c4478975e6478fa147d85209
ofietze <f.oliver@live.de>
2022-01-16 23:49:10 +0100

    Dirty proof of concept done

--- JS/main.js
+++ JS/main.js
@@ -39,6 +39,15 @@ function dateToHours(date) {
     return "".concat(date.getHours(), ":", date.getMinutes());
 }
 
+var position;
+var sunset = new Date();
+var sunrise = new Date();
+
+function main(){
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+}
+
 function success(pos) {
     position = pos;
     sunset = new Date().sunset(position.coords.latitude, position.coords.longitude);
@@ -47,19 +56,21 @@ function success(pos) {
     console.log("Sunset: " + sunset);
     document.getElementById(sunsetName).innerHTML = dateToHours(sunset);
     document.getElementById(sunriseName).innerHTML = dateToHours(sunrise);
+    const daylightHours = (sunset.getHours() * 60 + sunset.getMinutes()) - (sunrise.getHours() *60 + sunrise.getMinutes());
+    const percentOfDay = daylightHours / (24 * 60);
+    const dayLightDegrees = Math.round(percentOfDay * 360);
+    const sunriseDegrees = Math.round((((sunrise.getHours() * 60 + sunrise.getMinutes())/(24*60))*360 + 180) % 360);
+    console.log(daylightHours / 60 + " " + percentOfDay + "%");
+    console.log(daylightHours);
+    console.log(percentOfDay);
+    console.log(dayLightDegrees);
+    console.log(sunriseDegrees);
+    const styles = "width: 100%;height: 100%;display: flex; justify-content: center; align-items: center;border-radius: 50%;";
+    document.getElementById("sun-bg").setAttribute("style", styles + "background-image: conic-gradient(#ffbc009c " + dayLightDegrees+ "deg, #00000000 1deg); transform:rotate("+ sunriseDegrees + "deg);");
 }
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-var position;
-var sunset = new Date();
-var sunrise = new Date();
-
-function main(){
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
 }
 
 main();
